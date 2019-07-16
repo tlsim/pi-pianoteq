5a38f7387c57e35db1d3f933bc68ad033f0067c9
Tom Simmons <tom@ff8va.com>
2019-07-16 17:30:53 +0100

    Initial cmake rtmidi experiment.

---
+++ Probe.h
@@ -0,0 +1,15 @@
+//
+// Created by tsimmons on 09/07/2019.
+//
+
+#ifndef RT_MIDI_TEST_PROBE_H
+#define RT_MIDI_TEST_PROBE_H
+
+
+class Probe {
+public:
+    static int probe_ports();
+};
+
+
+#endif //RT_MIDI_TEST_PROBE_H
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Probe.h"
+#include "rtmidi-tests/midiprobe.cpp"
+/*int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}*/
+
+/*int main() {
+    try {
+        RtMidiIn midiin;
+    } catch (RtMidiError &error) {
+
+        error.printMessage();
+    }
+    return 0;
+}*/
+
+/*
+int main() {
+    //Probe::probe_ports();
+
+    return 0;
+}*/
