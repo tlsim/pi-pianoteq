17d7ed79bfc0647be9a0ffaaabccaf21e1973703
Tom Simmons <tom@ff8va.com>
2019-08-08 22:14:46 +0100

    Mapping and Row classes

--- src/midimapping/mapping.py
+++ src/midimapping/mapping.py
@@ -1,16 +1,49 @@
-from struct import *
-from typing import Optional
+from struct import pack
+from typing import Optional, List
+from functools import reduce
 
 HEADER_CONST = bytes.fromhex('19a3 e038')
 MAPPING_CONST = bytes.fromhex('0300 0000')
 ROW_CONST = bytes.fromhex('0100 0000')
-NOTES_CHANNEL = -1
-NOTES_TRANSPOSITION = 0
-MIDI_DIALECT = 0
-ROWS_TEMP = bytes.fromhex('0100 0000')  # TODO
+NOTES_CHANNEL_ANY = -1
+NOTES_TRANSPOSITION_ZERO = 0
+MIDI_DIALECT_AUTO = 3
 ENCODING = 'utf-8'
 
 # TODO: What do the mappings look like on ARM?
+
+
+class Mapping:
+    def __init__(self):
+        self.rows: List[Row] = []
+
+    def add_row(self, row):
+        self.rows.append(row)
+
+    def build(self) -> bytes:
+        mapping = MAPPING_CONST + \
+                  pack_c_long(NOTES_CHANNEL_ANY) + \
+                  pack_c_long(NOTES_TRANSPOSITION_ZERO) + \
+                  pack_c_long(MIDI_DIALECT_AUTO) + \
+                  pack_c_long(len(self.rows)) + \
+                  reduce(lambda x, y: x + y.build(), self.rows, b'')
+
+        return HEADER_CONST + pack_size(mapping) + mapping
+
+
+class Row:
+    def __init__(self, map_from: str, map_to: str):
+        self.map_from = map_from
+        self.map_to = map_to
+
+    def build(self) -> bytes:
+        map_from_bytes = bytes(self.map_from, ENCODING)
+        map_to_bytes = bytes(self.map_to, ENCODING)
+        return ROW_CONST + \
+            pack_size(map_from_bytes) + \
+            map_from_bytes + \
+            pack_size(map_to_bytes) + \
+            map_to_bytes
 
 
 def pack_c_long(i: int) -> bytes:
@@ -32,30 +65,12 @@ def map_to_load_preset(preset: str):
     return f'{{LoadPreset|28||{preset}|0}}'
 
 
-def create_row(map_from: str, map_to: str) -> bytes:
-    map_from_bytes = bytes(map_from, ENCODING)
-    map_to_bytes = bytes(map_to, ENCODING)
-    return ROW_CONST + \
-        pack_size(map_from_bytes) + \
-        map_from_bytes + \
-        pack_size(map_to_bytes) + \
-        map_to_bytes
-
-
-def build() -> bytes:
-    mapping = MAPPING_CONST + \
-        pack_c_long(NOTES_CHANNEL) + \
-        pack_c_long(NOTES_TRANSPOSITION) + \
-        pack_c_long(MIDI_DIALECT) + \
-        ROWS_TEMP + \
-        create_row(map_from_pc(10, 5), map_to_load_preset('Toy Piano Original'))
-
-    return HEADER_CONST + pack_size(mapping) + mapping
-
-
 if __name__ == '__main__':
-    output = build()
-    print(output)
-    print(output.hex())
+    test_row = Row(map_from_pc(10, 5), map_to_load_preset('Toy Piano Original'))
+    test_row_2 = Row(map_from_pc(11), map_to_load_preset('MKI Bark'))
+    test_mapping = Mapping()
+    test_mapping.add_row(test_row)
+    test_mapping.add_row(test_row_2)
+    output = test_mapping.build()
     with open('test_write_mapping.ptm', 'wb') as outfile:
         outfile.write(output)
