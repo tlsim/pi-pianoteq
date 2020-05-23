6562f6a0b4c687ed89495b7bb3cf372ee4e92440
Tom Simmons <tom@ff8va.com>
2020-05-23 14:15:19 +0100

    Start on gfxhat client

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ ssh pi@raspberrypi sudo chmod 644 /etc/systemd/system/pi_pianoteq.service
 ssh pi@raspberrypi pip3 install /home/pi/pi_pianoteq-1.0.0
 
 ssh pi@raspberrypi sudo systemctl daemon-reload
-ssh pi@raspberrypi sudo systemctl start pi_pianoteq
+ssh pi@raspberrypi sudo systemctl restart pi_pianoteq
 
 ssh pi@raspberrypi rm -r /tmp/pi_pianoteq*
 ssh pi@raspberrypi rm -r /home/pi/pi_pianoteq-1.0.0
