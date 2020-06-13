61a66b6c34f2f133952b76361554c6299bda94a9
Tom Simmons <tom@ff8va.com>
2020-06-13 18:52:49 +0100

    Refactor name

--- pi_pianoteq/config.py
+++ pi_pianoteq/config.py
@@ -11,16 +11,16 @@ INSTRUMENT_SECTION = 'Instrument'
 
 
 class Config:
-    config = ConfigParser()
-    config.read(path.join(path.dirname(__file__), CONFIG_FILE))
-    PIANOTEQ_DIR = config.get(PTQ_SECTION, 'PIANOTEQ_DIR')
-    PIANOTEQ_BIN = config.get(PTQ_SECTION, 'PIANOTEQ_BIN')
-    PIANOTEQ_DATA_DIR = config.get(PTQ_SECTION, 'PIANOTEQ_DATA_DIR')
-    PIANOTEQ_MIDI_MAPPINGS_DIR = config.get(PTQ_SECTION, 'PIANOTEQ_MIDI_MAPPINGS_DIR')
-    PIANOTEQ_PREFS_FILE = config.get(PTQ_SECTION, 'PIANOTEQ_PREFS_FILE')
-    MIDI_PORT_NAME = config.get(MIDI_SECTION, 'MIDI_PORT_NAME')
-    MIDI_MAPPING_NAME = config.get(MIDI_SECTION, 'MIDI_MAPPING_NAME')
-    UNKNOWN_INSTRUMENT = config.get(INSTRUMENT_SECTION, 'UNKNOWN_INSTRUMENT')
+    parser = ConfigParser()
+    parser.read(path.join(path.dirname(__file__), CONFIG_FILE))
+    PIANOTEQ_DIR = parser.get(PTQ_SECTION, 'PIANOTEQ_DIR')
+    PIANOTEQ_BIN = parser.get(PTQ_SECTION, 'PIANOTEQ_BIN')
+    PIANOTEQ_DATA_DIR = parser.get(PTQ_SECTION, 'PIANOTEQ_DATA_DIR')
+    PIANOTEQ_MIDI_MAPPINGS_DIR = parser.get(PTQ_SECTION, 'PIANOTEQ_MIDI_MAPPINGS_DIR')
+    PIANOTEQ_PREFS_FILE = parser.get(PTQ_SECTION, 'PIANOTEQ_PREFS_FILE')
+    MIDI_PORT_NAME = parser.get(MIDI_SECTION, 'MIDI_PORT_NAME')
+    MIDI_MAPPING_NAME = parser.get(MIDI_SECTION, 'MIDI_MAPPING_NAME')
+    UNKNOWN_INSTRUMENT = parser.get(INSTRUMENT_SECTION, 'UNKNOWN_INSTRUMENT')
 
     @staticmethod
     def load_instrument_config() -> List[str]:
