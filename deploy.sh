1ed289349216ce8802589087931e4ab28eb00aaa
Tom Simmons <tom@tomsimmons.dev>
2021-01-16 21:51:03 +0000

    Tidy deploy script, use mDNS hostname instead for new router without local DNS

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
-ssh pi@raspberrypi sudo rm -f /etc/systemd/system/pi_pianoteq.service
+REMOTE=pi@raspberrypi.local
 
-scp dist/pi_pianoteq-1.0.0.tar.gz pi@raspberrypi:/tmp/pi_pianoteq-1.0.0.tar.gz
-scp pi_pianoteq.service pi@raspberrypi:/tmp/pi_pianoteq.service
+# ssh pi@raspberrypi sudo rm -f /etc/systemd/system/pi_pianoteq.service
 
-ssh pi@raspberrypi tar -xvf /tmp/pi_pianoteq-1.0.0.tar.gz
+scp dist/pi_pianoteq-1.0.0.tar.gz $REMOTE:/tmp/pi_pianoteq-1.0.0.tar.gz
+scp pi_pianoteq.service $REMOTE:/tmp/pi_pianoteq.service
 
-ssh pi@raspberrypi sudo mv /tmp/pi_pianoteq.service /etc/systemd/system/pi_pianoteq.service
-ssh pi@raspberrypi sudo chmod 644 /etc/systemd/system/pi_pianoteq.service
+ssh $REMOTE <<EOF
+tar -xvf /tmp/pi_pianoteq-1.0.0.tar.gz
 
-ssh pi@raspberrypi pip3 install /home/pi/pi_pianoteq-1.0.0
+sudo mv /tmp/pi_pianoteq.service /etc/systemd/system/pi_pianoteq.service
+sudo chmod 644 /etc/systemd/system/pi_pianoteq.service
 
-ssh pi@raspberrypi sudo systemctl daemon-reload
-ssh pi@raspberrypi sudo systemctl restart pi_pianoteq
+pip3 install /home/pi/pi_pianoteq-1.0.0
 
-ssh pi@raspberrypi rm -r /tmp/pi_pianoteq*
-ssh pi@raspberrypi rm -r /home/pi/pi_pianoteq-1.0.0
+sudo systemctl daemon-reload
+sudo systemctl restart pi_pianoteq
+
+rm -r /tmp/pi_pianoteq*
+rm -r /home/pi/pi_pianoteq-1.0.0
+
+EOF
